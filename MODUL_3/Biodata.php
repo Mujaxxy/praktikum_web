32d97b9e9da215b2bb61010ed40a48432cdbdfa5
MUHAMMAD MUZAKKY <zakkysaja990@gmail.com>
2026-05-17 09:47:55 +0700

    first commit

---
+++ MODUL_3/Biodata.php
@@ -0,0 +1,11 @@
+<?php
+$nim = "202457201051";
+$nama = "Muzakky";
+$alamat = "Nganjuk";
+$nilai = "100";
+
+echo "NIM : $nim <br>";
+echo "Nama : $nama <br>";
+echo "Alamat : $alamat <br>";
+echo "Nilai : $nilai <br>";
+?>
